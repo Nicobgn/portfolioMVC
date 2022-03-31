c46291443520975fc56e9880ba30e4939181cafb
Niicoojk <nicobarragan_@hotmail.com>
2022-03-31 19:38:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 3000;
+
+const routesMain = require(path.join(__dirname + "/routes/main"));
+const routesUser = require(path.join(__dirname + "/routes/user"));
+const routesProducts = require(path.join(__dirname + "/routes/products"));
+
+//	Settings
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views/"));
+app.use(express.static("public"));
+
+// 	Routes
+app.use("/", routesMain);
+app.use("/user", routesUser);
+app.use("/store", routesProducts);
+
+// 	Middlewares
+app.use((req, res, next) => {
+  console.log(`En ${req.url} se utilizó ${req.method}`);
+  next();
+});
+
+// 	Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
